2c99ca23b9dccd9caedf9cc26011a5267a6a0817
Juan David Osorio Ortiz <jdoo1115@gmail.com>
2023-08-20 13:00:58 -0500

    use render props pattern, and support to render fn

--- src/components/TodoList/index.jsx
+++ src/components/TodoList/index.jsx
@@ -1,9 +1,24 @@
 import './TodoList.css';
 
-export default function TodoList({ children }) {
+export default function TodoList(props) {
+  const renderFn = props.children || props.render;
   return (
     <ul>
-      {children}
+      { props.error && props.onError() }
+      { props.loading && props.onLoading() }
+      { (
+          !props.loading && 
+          (props.search.length === 0) && 
+          (props.todosLength === 0)
+        ) && props.onEmptyTodos('There is no TODOS yet') 
+      }
+      { (
+          !props.loading && 
+          (props.search.length > 0) && 
+          (props.todosLength === 0)
+        ) && props.onEmptyTodos(`There are not TODOS for ${props.search}`)
+      }
+      { (props.todos && !props.loading && !props.error) && props.todos.map(renderFn) }
     </ul>
   )
 }

bb83753b72df5e6c766581f43e60ae29732e552d
Juan David Osorio Ortiz <jdoo1115@gmail.com>
2023-08-20 12:58:27 -0500

    add loading status

--- src/components/TodoCounter/index.jsx
+++ src/components/TodoCounter/index.jsx
@@ -1,7 +1,9 @@
-export default function TodoCounter ({ total, completed }) {
+import './TodoCounter.css';
+
+export default function TodoCounter ({ total, completed, loading }) {
   return (
     <>
-      <h2 style={{ margin: 0 }}>
+      <h2 className={`${loading && 'loading'}`} style={{ margin: 0 }}>
         {completed} TODO <br /> completed of {total}
       </h2>
     </>
--- src/components/TodoSearch/index.jsx
+++ src/components/TodoSearch/index.jsx
@@ -1,13 +1,19 @@
 import { useRef } from 'react';
 import './TodoSearch.css';
 
-export default function TodoSearch({ search, setSearch }) {
+export default function TodoSearch({ search, setSearch, loading }) {
   const ref = useRef(search)
   const handleChange = (e) => {
     const value = e.target.value;
     setSearch(value);
   }
   return (
-    <input ref={ref} type="search" placeholder="Search a todo" onChange={ handleChange }/>
+    <input 
+      ref={ref} 
+      type="search" 
+      placeholder="Search a todo" 
+      onChange={ handleChange }
+      disabled={loading}
+    />
   )
 }
